85bfdd9d8dffa4e4e292e0f442c792403d6deba7
khader huijih <164516684+khaderGg@users.noreply.github.com>
2025-01-17 19:05:07 +0300

    adddetals

--- lib/Screens/FinancialAnalysis.dart
+++ lib/Screens/FinancialAnalysis.dart
@@ -14,12 +14,11 @@ class Financialanalysis extends StatefulWidget {
 }
 
 class _FinancialanalysisState extends State<Financialanalysis> {
-  List<SalesData> expenseData = [];
-  List<SalesData> incomeData = [];
+  List<Map<String, dynamic>> expenses = [];
+  List<Map<String, dynamic>> incomes = [];
   double totalExpenses = 0;
   double totalIncome = 0;
   bool isLoading = true;
-  String selectedTimePeriod = 'Last Month'; // Default selection
 
   @override
   void initState() {
@@ -30,23 +29,23 @@ class _FinancialanalysisState extends State<Financialanalysis> {
   loadData() async {
     final sharedPreferences = await SharedPreferences.getInstance();
 
-    // جلب بيانات المصاريف بناءً على الفترة الزمنية المختارة
+    // جلب بيانات المصاريف باستخدام getExpenses
     SharedPreferencesServiceexpenses(sharedPreferences)
-        .getExpensesForTimePeriod(selectedTimePeriod)
-        .then((expenses) {
+        .getExpenses()
+        .then((expenseData) {
       setState(() {
-        expenseData = expenses;
-        totalExpenses = expenses.fold(0, (sum, item) => sum + item.sales);
+        expenses = expenseData;
+        totalExpenses = expenses.fold(0, (sum, item) => sum + item['value']);
       });
     });
 
-    // جلب بيانات المداخيل بناءً على الفترة الزمنية المختارة
+    // جلب بيانات المداخيل باستخدام getIncomes
     SharedPreferencesServiceIncomes(sharedPreferences)
-        .getIncomesForTimePeriod(selectedTimePeriod)
-        .then((incomes) {
+        .getIncomes()
+        .then((incomeData) {
       setState(() {
-        incomeData = incomes;
-        totalIncome = incomes.fold(0, (sum, item) => sum + item.sales);
+        incomes = incomeData;
+        totalIncome = incomes.fold(0, (sum, item) => sum + item['value']);
         isLoading = false; // البيانات جاهزة الآن
       });
     });
@@ -63,158 +62,186 @@ class _FinancialanalysisState extends State<Financialanalysis> {
             icon: Icon(Icons.arrow_back)),
         title: const Text("Financial Analysis Chart"),
         centerTitle: true,
-        backgroundColor: Colors.green[700],
+        backgroundColor: Colors.teal[700],
       ),
       body: isLoading
           ? const Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                // إضافة قائمة منسدلة لاختيار الفترة الزمنية
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: DropdownButton<String>(
-                    value: selectedTimePeriod,
-                    onChanged: (String? newValue) {
-                      setState(() {
-                        selectedTimePeriod = newValue!;
-                        isLoading = true; // إعادة تحميل البيانات
-                        loadData(); // تحميل البيانات بناءً على الفترة الزمنية الجديدة
-                      });
-                    },
-                    items: <String>['Last Week', 'Last Month']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                ),
-
-                // عرض المخطط فقط إذا كانت البيانات موجودة
-                if (expenseData.isNotEmpty || incomeData.isNotEmpty)
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 20),
-                      child: SfCartesianChart(
-                        primaryXAxis: CategoryAxis(),
-                        primaryYAxis: NumericAxis(
-                            minimum: 0,
-                            maximum: (totalIncome + totalExpenses) * 1.1,
-                            interval: (totalIncome + totalExpenses) / 10),
-                        title: ChartTitle(
-                            text: 'Financial Data ($selectedTimePeriod)'),
-                        legend: Legend(isVisible: true),
-                        tooltipBehavior: TooltipBehavior(enable: true),
-                        series: <CartesianSeries>[
-                          ColumnSeries<SalesData, String>(
-                            dataSource: expenseData,
-                            xValueMapper: (SalesData sales, _) => sales.month,
-                            yValueMapper: (SalesData sales, _) => sales.sales,
-                            name: 'Expenses',
-                            color: Colors.red,
-                            dataLabelSettings:
-                                const DataLabelSettings(isVisible: true),
-                          ),
-                          ColumnSeries<SalesData, String>(
-                            dataSource: incomeData,
-                            xValueMapper: (SalesData sales, _) => sales.month,
-                            yValueMapper: (SalesData sales, _) => sales.sales,
-                            name: 'Income',
-                            color: Colors.green,
-                            dataLabelSettings:
-                                const DataLabelSettings(isVisible: true),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                else
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Text(
-                      'No data available for this period. Please add some data first.',
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red),
-                    ),
-                  ),
-
-                // مخطط دائري للمقارنة بين المصاريف والمداخيل إذا كانت البيانات موجودة
-                if (expenseData.isNotEmpty || incomeData.isNotEmpty)
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 10, vertical: 20),
-                      child: SfCircularChart(
-                        title:
-                            ChartTitle(text: 'Income vs Expenses (Pie Chart)'),
-                        legend: Legend(
-                          isVisible: true,
-                          overflowMode: LegendItemOverflowMode.wrap,
-                        ),
-                        series: <CircularSeries>[
-                          PieSeries<SalesData, String>(
-                            dataSource: [
-                              SalesData('Expenses', totalExpenses),
-                              SalesData('Income', totalIncome),
-                            ],
-                            xValueMapper: (SalesData data, _) => data.month,
-                            yValueMapper: (SalesData data, _) => data.sales,
-                            dataLabelSettings:
-                                const DataLabelSettings(isVisible: true),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                else
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Text(
-                      'No data available for this period. Please add some data first.',
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red),
-                    ),
-                  ),
-
-                // عرض الفارق بين المصاريف والمداخيل إذا كانت البيانات موجودة
-                if (expenseData.isNotEmpty || incomeData.isNotEmpty)
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Difference: ${(totalIncome - totalExpenses).toStringAsFixed(2)}',
-                      style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blue),
-                    ),
-                  ),
-                // عرض إجمالي المصاريف والمداخيل إذا كانت البيانات موجودة
-                if (expenseData.isNotEmpty || incomeData.isNotEmpty)
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Total Expenses: ${totalExpenses.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                if (expenseData.isNotEmpty || incomeData.isNotEmpty)
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Total Income: ${totalIncome.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-              ],
+          : SingleChildScrollView(  // جعل الصفحة قابلة للتمرير
+        child: Column(
+          children: [
+            // عرض الفرق المالي في بداية الصفحة
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Financial Difference: ${(totalIncome - totalExpenses).toStringAsFixed(2)}',
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: totalIncome >= totalExpenses
+                        ? Colors.green
+                        : Colors.red),
+              ),
             ),
+
+            // عرض المخطط البياني (Bar Chart) مع الألوان المحترفة
+            if (totalExpenses > 0 || totalIncome > 0)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: SfCartesianChart(
+                  primaryXAxis: CategoryAxis(),
+                  primaryYAxis: NumericAxis(
+                    minimum: 0,
+                    maximum: (totalIncome + totalExpenses) * 1.1,
+                    interval: (totalIncome + totalExpenses) / 10,
+                  ),
+                  title: ChartTitle(
+                      text: 'Financial Data (Total Income vs Expenses)',
+                      textStyle: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.teal[700],
+                      )),
+                  legend: Legend(
+                      isVisible: true,
+                      position: LegendPosition.bottom,
+                      backgroundColor: Colors.transparent),
+                  tooltipBehavior: TooltipBehavior(enable: true),
+                  series: <CartesianSeries>[
+                    ColumnSeries<SalesData, String>(
+                      dataSource: [
+                        SalesData('Expenses', totalExpenses),
+                        SalesData('Income', totalIncome),
+                      ],
+                      xValueMapper: (SalesData sales, _) => sales.month,
+                      yValueMapper: (SalesData sales, _) => sales.sales,
+                      name: 'Financial Data',
+                      color: Colors.red.withOpacity(0.7),
+                      dataLabelSettings: DataLabelSettings(isVisible: true),
+                    ),
+                  ],
+                ),
+              )
+            else
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  'No data available. Please add some data first.',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
+                ),
+              ),
+
+            // عرض المخطط الدائري (Pie Chart)
+            if (totalExpenses > 0 || totalIncome > 0)
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: SfCircularChart(
+                  title: ChartTitle(
+                    text: 'Income vs Expenses',
+                    textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal[700],
+                    ),
+                  ),
+                  legend: Legend(
+                    isVisible: true,
+                    overflowMode: LegendItemOverflowMode.wrap,
+                    position: LegendPosition.bottom,
+                    backgroundColor: Colors.transparent,
+                  ),
+                  series: <CircularSeries>[
+                    PieSeries<SalesData, String>(
+                      dataSource: [
+                        SalesData('Expenses', totalExpenses),
+                        SalesData('Income', totalIncome),
+                      ],
+                      xValueMapper: (SalesData data, _) => data.month,
+                      yValueMapper: (SalesData data, _) => data.sales,
+                      dataLabelSettings: const DataLabelSettings(isVisible: true),
+                    ),
+                  ],
+                ),
+              )
+            else
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  'No data available. Please add some data first.',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
+                ),
+              ),
+
+            // عرض المصاريف والمداخيل أسفل الصفحة مع تأثير الخطوط
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Expenses and Incomes',
+                    style: TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  // عرض المصاريف
+                  if (expenses.isNotEmpty)
+                    ListView.builder(
+                      shrinkWrap: true,  // لتقليص حجم القائمة
+                      physics: NeverScrollableScrollPhysics(),  // تعطيل التمرير هنا
+                      itemCount: expenses.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Icon(
+                            Icons.arrow_downward,
+                            color: Colors.red,
+                          ),
+                          title: Text(
+                            '${expenses[index]["type"]}: ${expenses[index]["value"]}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          subtitle: Text(
+                            'Date: ${expenses[index]["date"]}',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        );
+                      },
+                    ),
+                  const SizedBox(height: 10),
+                  // عرض المداخيل
+                  if (incomes.isNotEmpty)
+                    ListView.builder(
+                      shrinkWrap: true,  // لتقليص حجم القائمة
+                      physics: NeverScrollableScrollPhysics(),  // تعطيل التمرير هنا
+                      itemCount: incomes.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Icon(
+                            Icons.arrow_upward,
+                            color: Colors.green,
+                          ),
+                          title: Text(
+                            '${incomes[index]["type"]}: ${incomes[index]["value"]}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          subtitle: Text(
+                            'Date: ${incomes[index]["date"]}',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        );
+                      },
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
